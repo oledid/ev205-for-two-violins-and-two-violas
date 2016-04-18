c627160f453c8816b1c2502391dc256ca9b63259
Ole Morten Didriksen <code@oledid.com>
2016-04-18 16:20:00 +0200

    Hardcode default win32 lilypond install-path for now

--- Gulpfile.js
+++ Gulpfile.js
@@ -5,13 +5,18 @@ var shell = require("gulp-shell");
 var filename = "ev205.ly";
 var arguments = "--pdf --png --output build/ev205";
 
+var exeFile = "lilypond";
+if (process.platform === "win32") {
+	exeFile = "\"C:/Program Files (x86)/LilyPond/usr/bin/lilypond.exe\"";
+}
+
 gulp.task("default", function() {
     gulp.start("build");
 });
 
 gulp.task("build", shell.task([
     "mkdir build",
-    "lilypond " + arguments + " " + filename
+    exeFile + " " + arguments + " " + filename
 ], {
     ignoreErrors: true
 }));
